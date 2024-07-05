365627e76eba53a21dbe30bd1a903eb48ebeb4ed
Sebastián Serrisuela <sebaserri@gmail.com>
2024-07-05 18:16:31 -0300

    Creo el servicio y la estructura html para obtener y mostrar la info del usuario.

---
+++ src/app/signals/services/userService.service.ts
@@ -0,0 +1,22 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable, tap, map } from 'rxjs';
+import { SingleUserResponse, User } from '../interfaces/user-request.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  private http = inject( HttpClient );
+  private baseUrl: string = 'https://reqres.in/api/users/';
+
+  getUserById( id: number): Observable<User>{
+    return this.http.get<SingleUserResponse>(`${ this.baseUrl }/${ id }`)
+      .pipe(
+        map( response => response.data ),
+        tap( console.log ),
+      );
+
+  }
+}
