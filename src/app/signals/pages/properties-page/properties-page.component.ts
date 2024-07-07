eb302a629441e29d5fec2782750fca1b44f1792d
Sebastián Serrisuela <sebaserri@gmail.com>
2024-07-07 15:31:45 -0300

    Actualizando parte de estado de un usuario.

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
@@ -7,7 +8,49 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
-public onFieldUpdated(field: string, value: any): void {
-    console.log(`El campo '${field}' se ha actualizado a ${value}`);
+  public user = signal<User>({
+    id: 1,
+    email: "george.buth@reqres.in",
+    first_name: "George",
+    last_name: "Buth",
+    avatar: "https://reqres.in/img/faces/1-image.jpg"
+  });
+
+  public fullName = computed( () => `${ this.user().first_name} ${ this.user().last_name}` );
+
+  public onFieldUpdated(field: keyof User, value: any): void {
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value,
+    // });
+
+    // this.user.update( current => ({
+    //   ...current,
+    //   [field]: value
+    // }) );
+
+    this.user.update( current => {
+
+      switch(field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number( value );
+      }
+
+      return current;
+    } );
   }
+
 }
+
